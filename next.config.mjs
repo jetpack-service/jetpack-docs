bd1f7494902279518932f5d0edeb012d05ecaa60
jetpack-service <jetpack@combineads.co.kr>
2024-11-11 19:36:22 +0800

    Add support for multiple languages

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,15 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 export default withBundleAnalyzer(withNextra({
   i18n: {
-    locales: ['en', 'ko'],
+    locales: ['en', 'ko', 'af', 'ak', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be',
+      'bn', 'bi', 'bs', 'br', 'bg', 'my', 'ca', 'ny', 'zh-Hans', 'zh-Hant',
+      'hr', 'cs', 'da', 'fa-AF', 'dv', 'nl', 'eo', 'et', 'ee', 'fil', 'fi',
+      'fr', 'gl', 'ka', 'de', 'ki', 'el', 'gu', 'ha', 'he', 'hi', 'hu', 'ig',
+      'id', 'it', 'ja', 'kn', 'kk', 'km', 'ku', 'ky', 'lv', 'lt', 'lb', 'mk',
+      'mai', 'ms', 'ml', 'mr', 'mn', 'ne', 'pcm', 'no', 'or', 'ps', 'fa', 'pl',
+      'pt', 'pt-BR', 'pa', 'ro', 'ru', 'sat', 'sr-Latn', 'sn', 'si', 'sk', 'sl',
+      'so', 'es', 'sw', 'sv', 'tl', 'tg', 'ta', 'te', 'th', 'ti', 'tr', 'tk',
+      'uk', 'ur-IN', 'ur-PK', 'uz', 'vi', 'yo', 'zu'],
     defaultLocale: 'en'
   },
   reactStrictMode: true
